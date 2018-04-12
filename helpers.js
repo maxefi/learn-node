aa7729fee89702bb7bf4f677f63426266da3cdff
maxefi <maxefi@mail.ru>
2018-04-12 18:42:12 +0300

    review pug, review use helpers with middleware pattern, decompose storeController from route, fix scss import error

--- helpers.js
+++ helpers.js
@@ -5,7 +5,8 @@
 // FS is a built in module to node that let's us read files from the system we're running on
 const fs = require('fs');
 
-// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5 minutes ago"
+// moment.js is a handy library for displaying dates. We need this in our templates to display things like "Posted 5
+// minutes ago"
 exports.moment = require('moment');
 
 // Dump is a handy debugging function we can use to sort of "console.log" our data
@@ -18,12 +19,13 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
+exports.title = 'hui';
 exports.siteName = `Rise And Fall`;
 
 exports.menu = [
-  { slug: '/stores', title: 'Stores', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/map', title: 'Map', icon: 'map', },
+    { slug: '/stores', title: 'Stores', icon: 'store', },
+    { slug: '/tags', title: 'Tags', icon: 'tag', },
+    { slug: '/top', title: 'Top', icon: 'top', },
+    { slug: '/add', title: 'Add', icon: 'add', },
+    { slug: '/map', title: 'Map', icon: 'map', },
 ];
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeConroller');
 
-// Do work here
-router.get('/', (req, res) => {
-    const max = { name: 'Max', age: '25', cool: false };
-    // res.send('Sup, dude. Nothing works...');
-    // res.json(max);
-    // res.send(req.query.name);
-    res.json(req.query);
-});
+router.get('/', storeController.homePage);
 
 router.get('/reverse/:name', (req, res) => {
     res.send([...req.params.name].reverse().join(''));
