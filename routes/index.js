2def68b829bec4e28cb11781bded6b281cc71e84
maxefi <maxefi@mail.ru>
2018-04-12 17:38:20 +0300

    update nodemon, .gitignore and name in helpers.js, playing with routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+    const max = { name: 'Max', age: '25', cool: false };
+    // res.send('Sup, dude. Nothing works...');
+    // res.json(max);
+    // res.send(req.query.name);
+    res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+    res.send([...req.params.name].reverse().join(''));
 });
 
 module.exports = router;
