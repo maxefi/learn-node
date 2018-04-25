d87a1d57e1cf5b6f3d80c536c312f548035d2ae6
maxefi <maxefi@mail.ru>
2018-04-25 15:09:00 +0300

    create edit/update flow for stores
    - update _storeCard template;
    - add get/post routes for store with _id as req.params;
    - add editStore in controller for showing editing store data, update _storeForm template to actually show stores data;
    - add updateStore in controller for saving edited store data and show flash;

--- controllers/storeConroller.js
+++ controllers/storeConroller.js
@@ -20,3 +20,23 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the ID
+    const store = await Store.findOne({ _id: req.params.id });
+    // TODO: 2. confirm they are the owner of the store
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name} Store`, store });
+};
+
+exports.updateStore = async (req, res) => {
+    // 1. Find and update store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // return the new store instead of the old one
+        runValidators: true,
+    }).exec();
+    const { name, slug, _id } = store;
+    req.flash('success', `Successfully updated <strong>${name}</strong>. <a href='/stores/${slug}'>View Store -></a>`);
+    // 2. Redirect them the store and tell it worked
+    res.redirect(`/stores/${_id}/edit`);
+};
