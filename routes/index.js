9df03783cfe83050651d4f2491ec9be7e4485110
maxefi <maxefi@mail.ru>
2018-04-25 14:10:32 +0300

    query the DB for a list of all stores
    - update routes;
    - create stores page layout and storeCard mixin;

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const storeController = require('../controllers/storeConroller');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage);
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-// composition - function wrapped with a function
 router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
