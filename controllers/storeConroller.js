c20fd2dc12317c066845a84ba426f96ef1799f29
maxefi <maxefi@mail.ru>
2018-04-25 12:43:18 +0300

    add create store controller with cb/promise/async examples, use middleware to catch errors

--- controllers/storeConroller.js
+++ controllers/storeConroller.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     console.log('req.name: ', req.name);
     res.render('index');
@@ -7,7 +10,30 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-    console.log('req.body: ', req.body); // see data posted
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    // see data posted
+    // console.log('req.body: ', req.body);
+
+    const store = new Store(req.body);
+
+    // sync example with a callback
+    // store.save(function(err, store) {
+    //     if(!err) return res.redirect('/');
+    // });
+
+    // sync example with promises
+    // store.save()
+    //      .then(store => {
+    //          res.json(store);
+    //      })
+    //      .then(stores => {
+    //          res.render('storeList', { stores });
+    //      })
+    //      .catch(err => {
+    //          throw Error(err);
+    //      });
+
+    // should be try/catched, instead create hof 'catchErrors' for it
+    await store.save();
+    res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeConroller');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+// composition - function wrapped with a function
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
