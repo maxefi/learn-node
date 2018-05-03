df583bb551e4d0575aa6bc209767097aa8a14844
maxefi <maxefi@mail.ru>
2018-05-03 12:02:17 +0300

    add Tags page
    - create explicit get routes for Tags page, which might be replaced with just one, by using *? (optional) parameter;
    - create mongodb getTagsList aggregation to get the list of used tags with its usage count;
    - add storeController method and layout template to get and render those tags;

--- controllers/storeConroller.js
+++ controllers/storeConroller.js
@@ -77,3 +77,9 @@ exports.getStoreBySlug = async (req, res, next) => {
     if (!store) return next();
     res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res, next) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag;
+    res.render('tags', { tags, title: 'Tags', tag });
+};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const storeController = require('../controllers/storeConroller');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-const { getStores, addStore, createStore, updateStore, editStore, upload, resize, getStoreBySlug } = storeController;
+const {
+    getStores, addStore, createStore, updateStore, editStore, upload, resize, getStoreBySlug,
+    getStoresByTag,
+} = storeController;
 
 router.get('/', catchErrors(getStores));
 router.get('/stores', catchErrors(getStores));
@@ -11,6 +14,8 @@ router.get('/add', addStore);
 router.post('/add', upload, catchErrors(resize), catchErrors(createStore));
 router.post('/add/:id', upload, catchErrors(resize), catchErrors(updateStore));
 router.get('/stores/:id/edit', catchErrors(editStore));
-router.get('/store/:slug', catchErrors(getStoreBySlug))
+router.get('/store/:slug', catchErrors(getStoreBySlug));
+router.get('/tags', catchErrors(getStoresByTag));
+router.get('/tags/:tag', catchErrors(getStoresByTag));
 
 module.exports = router;
