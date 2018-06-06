38eec3473948f61c963005eb7b9d5230507d7122
maxefi <maxefi@mail.ru>
2018-06-06 19:37:26 +0300

    finish Reviews functionality
    - add virtual reviews field to the Store model and explicitly ask it to be revealed in JSON/Object;
    - update store template with _reviewItem for displaying actual reviews;

--- models/Store.js
+++ models/Store.js
@@ -40,6 +40,9 @@ const storeSchema = new mongoose.Schema({
         ref: 'User',
         required: 'Author is REQUIRED',
     },
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
 });
 
 // Define indexes
@@ -74,5 +77,14 @@ storeSchema.statics.getTagsList = function() {
         { $sort: { count: -1 } },
     ]);
 };
+
+// find reviews where the stores _id property === reviews store property
+// equals to SQL Join thing
+// virtual fields don't go to the object or json unless it's explicitly asked to
+storeSchema.virtual('reviews', {
+    ref: 'Review', // what model to link
+    localField: '_id', // which field on the store
+    foreignField: 'store', // which field on the store
+});
 
 module.exports = mongoose.model('Store', storeSchema);
